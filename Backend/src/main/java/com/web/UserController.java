5c95094ca747c07118b05d8c4a44b09e8f24fc65
jn.lee <jinam@socialbean.co.kr>
2020-05-27 12:32:26 +0900

    [WIP] JWT 를 이용한 User 인증 구현 중

---
+++ Backend/src/main/java/com/web/UserController.java
@@ -0,0 +1,45 @@
+package com.web;
+
+import com.domain.user.User;
+import com.domain.user.UserRepository;
+import config.security.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import net.bytebuddy.asm.Advice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@RestController
+public class UserController {
+
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserRepository userRepository;
+
+    // 회원가입
+    @PostMapping("/join")
+    public Long join(@RequestBody Map<String, String> user) {
+        return userRepository.save(User.builder()
+                .email(user.get("email"))
+                .password(passwordEncoder.encode(user.get("password")))
+                .roles(Collections.singletonList("ROLE_USER")) // 최초 가입시 USER 로 설정
+                .build()).getId();
+    }
+
+    // 로그인
+    @PostMapping("/login")
+    public String login(@RequestBody Map<String, String> user) {
+        User member = userRepository.findByEmail(user.get("email"))
+                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 E-MAIL 입니다."));
+        if (!passwordEncoder.matches(user.get("password"), member.getPassword())) {
+            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
+        }
+        return jwtTokenProvider.createToken(member.getUsername(), member.getRoles());
+    }
+}
