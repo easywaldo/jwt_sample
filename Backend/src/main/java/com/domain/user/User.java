6879af0170372ba2f29813fe31be6b7cf0322ea6
jn.lee <jinam@socialbean.co.kr>
2020-05-27 10:31:32 +0900

    User Domain 이용한SpringSecurity Gradle 기본적용

---
+++ Backend/src/main/java/com/domain/user/User.java
@@ -0,0 +1,25 @@
+package com.domain.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 300, nullable = false)
+    private String password;
+}
